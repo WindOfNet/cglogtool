ff97e424bc6f5d2f13e709b1d8db92c7ca4ccb38
windofnet <windofnet@hotmail.com>
2023-11-17 11:38:22 +0800

    export image

--- src/lib/Table.svelte
+++ src/lib/Table.svelte
@@ -1,12 +1,32 @@
 <script lang="ts">
   import type { Column } from '$lib/types';
+  import domtoimage from 'dom-to-image';
 
   export let columns: Column[];
   export let data: Record<string, any>[];
+
+  let tableRef: HTMLElement;
+
+  function handleScreenshot() {
+    const clone = tableRef.cloneNode(true) as HTMLElement;
+    clone.setAttribute('data-theme', 'light');
+    clone.classList.add('max-w-[800px]');
+    document.body.appendChild(clone);
+    domtoimage.toPng(clone).then(function (dataUrl) {
+      clone.remove();
+      const w = window.open('');
+      var img = new Image();
+      img.src = dataUrl;
+      w?.document.write(img.outerHTML);
+    });
+  }
 </script>
 
+<div>
+  <button class="btn btn-primary" on:click={handleScreenshot}>匯出成圖片</button>
+</div>
 <div class="overflow-x-auto">
-  <table class="table w-full">
+  <table class="table w-full" bind:this={tableRef}>
     <thead>
       <tr>
         {#each columns as column}
